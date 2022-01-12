3eb0e67e170649b71d0b17f7df301a04a67a3b32
Wilson Lopez <wilson.lopez@jalasoft.com>
2022-01-12 14:25:39 -0400

    diamong

--- src/codewars/Give me a Diamond/index.js
+++ src/codewars/Give me a Diamond/index.js
@@ -1,3 +1,23 @@
 export const diamond = n => {
-  console.log("hj");
+  if (n <= 0 || n % 2 === 0) {
+    return null
+  }
+  let str = ''
+  for (let i = 0; i < n; i++) { 
+    let len = Math.abs((n-2*i-1)/2)
+    str += ' '.repeat(len)
+    str += '*'.repeat(n-2*len)
+    str += '\n'
+  }
+  return str
+}
+
+export const diamond2 = n => {
+  if (n % 2  == 0 || n < 1) return null
+  var x = 0, add, diam = line(x,n)
+  while((x+=2) < n) {
+    add = line(x/2, n-x)
+    diam = add+diam+add;
+  }
+  return diam;
 }
--- src/codewars/Give me a Diamond/index.test.js
+++ src/codewars/Give me a Diamond/index.test.js
@@ -1,5 +1,13 @@
 import { diamond } from './index'
 
+test('Give me a Diamond with null', () => {
+  expect(diamond('')).toBe(null)
+})
+
 test('Give me a Diamond with 5', () => {
-  expect(diamond)
+  expect(diamond(3)).toBe(" *\n***\n *\n")
+})
+
+test('Give me a Diamond with 5', () => {
+  expect(diamond(-3)).toBe(null)
 })
